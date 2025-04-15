3e64088fcc9710020f29664a63796fc85d6e8885
Sivasankari <sivasangarikalaiventh@gmail.com>
2025-04-15 07:57:47 +0530

    15-04-2025 local

---
+++ src/ex_01_javabasic/LocalL.java
@@ -0,0 +1,7 @@
+package ex_01_javabasic;
+
+public class LocalL {
+    public static void main(String[] args) {
+        System.out.println("local");
+    }
+}
---
+++ src/ex_01_javabasic/lab004_comments.java
@@ -0,0 +1,16 @@
+package ex_01_javabasic;
+
+public class lab004_comments {
+    //shjkdhgghj
+
+    /**
+     * jhryj
+     * rhtyjhtr
+     * dhyrhkj
+     * rtjktrj
+     **/
+
+
+//System.out.println("jfkdh");
+System.out.println("jfkdh");
+System.out.println("jfkdh");
