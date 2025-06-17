f9faea49e2c4d791c4c851bab7991ecf51567e8a
Sivasankari <sivasangarikalaiventh@gmail.com>
2025-06-17 09:00:44 +0530

    Ternary operator program

---
+++ src/ex_01_javabasic/practise.java
@@ -0,0 +1,11 @@
+package ex_01_javabasic;
+
+public class practise {
+    public static void main(String args[])
+    {
+        int a=9;
+        System.out.printf("%d*1=%d\n",a,a*1);
+
+    }
+
+}
