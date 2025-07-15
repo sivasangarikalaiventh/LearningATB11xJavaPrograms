bf37853e63f6705c96f0eb668f2253a8e8f22ad2
Sivasankari <sivasangarikalaiventh@gmail.com>
2025-07-15 06:54:47 +0530

    Fizz,Buzz program

---
+++ src/ex_01_javabasic/Forloop_tasks.java
@@ -0,0 +1,23 @@
+package ex_01_javabasic;
+
+public class Forloop_tasks {
+    public static void main(String[] args) {
+        for(int i=1;i<=100;i++)
+        {
+            if(i%3==0)
+            {
+                System.out.println("Fizz");
+            }else if(i%5==0)
+            {
+                System.out.println("Buzz");
+            }else if(i%3==0 && i%5==0)
+            {
+                System.out.println("FizzBuzz");
+            }
+            else
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
